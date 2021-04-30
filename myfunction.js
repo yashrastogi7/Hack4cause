24580c73f4eaaea2c639a48378a38554bf19014e
Aviral <aviralgupta044@gmail.com>
2021-04-30 18:40:34 +0530

    a

---
+++ myfunction.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var x = document.getElementById("Name").value;
+    var y= document.getElementById("Age").value;
+    if (x=="" || y=="") {
+        alert("Please Fill All Required Field");return false;
+    }
+    else
+    {
+        window.open("Hack4Cause.html");return false;
+    }
+    }
