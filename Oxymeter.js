1c544a6f633b3eff5260c330fa91c8eec7727a21
Aviral <aviralgupta044@gmail.com>
2021-05-02 03:38:49 +0530

    a

--- Oxymeter.js
+++ Oxymeter.js
@@ -1,8 +1,8 @@
 function pageRedirect() {
     var x = document.getElementById("Spo2").value;
     var y= document.getElementById("HeartRate").value;
-    if (x <=98 && y<=100) {
-        window.open("https://www.tutorialrepublic.com/");
+    if (x <=90) {
+        window.open("http://192.168.1.6/4/on");
     }
     else {document.getElementById("O2").innerHTML= "O2 Level Fine!"}
 }
